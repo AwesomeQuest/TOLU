8e2d303cb8874c2980576421913a8f0b5250a32f
Torfi Þorgrímsson <torfit@gmail.com>
2024-11-28 21:36:40 +0000

    lots of plots

--- skil2/calc.m
+++ skil2/calc.m
@@ -1,29 +1,22 @@
-Td = ((2:10) .*10 + 273.15)';
-mud = [1.003, 0.799, 0.657, 0.548, 0.467, 0.405, 0.355, 0.316, 0.283]'*1e-3;
-
-V0 = 0.01;
-T0 = 20 + 273.15;
-T1 = 80 + 273.15;
-L  = 0.1;
+T = ((2:10) .*10 + 273.15)';
+mu = [1.003, 0.799, 0.657, 0.548, 0.467, 0.405, 0.355, 0.316, 0.283]'*1e-3;
 
 
+f = @(c) exp(c(1)./T + c(2)*T + c(3)*T.^2) - mu;
+Df = @(c) [
+    1./T T T .^2
+    ] .* exp(c(1)./T + c(2)*T + c(3)*T.^2);
 
+betahat = newton(f,Df,zeros(3,1),1e-5)
+norm(f(betahat))
 
-simpson(@(x) x^3, 0,1,4)
+function u=newton(f,Df,x0,tol)
+    prev = x0;
+    curr = x0 - Df(x0)\f(x0);
 
-
-function I=simpson(f,a,b,n)
-    h=(b-a)/n;
-    x=a;y=f(a);
-    for i=1:n-1
-        x=x+h;
-        y=y+2*f(x);
+    while norm(curr - prev) > tol
+        prev = curr;
+        curr = curr - Df(curr)\f(curr);
     end
-    x=a;
-    for i=1:n
-        y=y+4*f((x+x+h)/2);
-        x=x+h;
-    end
-    y=y+f(b);
-    I=h/6*y;
+    u = curr;
 end
